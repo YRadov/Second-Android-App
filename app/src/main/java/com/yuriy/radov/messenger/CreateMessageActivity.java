089c1b4c2287457ed6b1631f667090d13d922a56
YRadov <radov.yuriy@ukr.net>
2017-02-14 21:19:16 +0200

    finish first activity

--- app/src/main/java/com/yuriy/radov/messenger/CreateMessageActivity.java
+++ app/src/main/java/com/yuriy/radov/messenger/CreateMessageActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class CreateMessageActivity extends AppCompatActivity {
 
@@ -11,13 +12,17 @@ public class CreateMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_message);
-    }
+    }//onCreate
 
     // Вызвать onSendMessage() при щелчке на кнопке
     public void onSendMessage(View view) {
+        //Получить ссылку на EditText
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+        // Создать и отправить интент
         Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
         startActivity(intent);
-    }
+    }//onSendMessage
 
-
-}
+}//CreateMessageActivity
