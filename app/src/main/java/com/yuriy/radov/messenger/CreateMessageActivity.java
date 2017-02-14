a23182b83ccac2d4ffea0caf10432603e129e00c
YRadov <radov.yuriy@ukr.net>
2017-02-14 21:47:28 +0200

    create implicit intent

--- app/src/main/java/com/yuriy/radov/messenger/CreateMessageActivity.java
+++ app/src/main/java/com/yuriy/radov/messenger/CreateMessageActivity.java
@@ -20,8 +20,13 @@ public class CreateMessageActivity extends AppCompatActivity {
         EditText messageView = (EditText) findViewById(R.id.message);
         String messageText = messageView.getText().toString();
         // Создать и отправить интент
-        Intent intent = new Intent(this, ReceiveMessageActivity.class);
-        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
+//        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+//        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
+
+        // Интент стороннему приложению
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, messageText);
         startActivity(intent);
     }//onSendMessage
 
